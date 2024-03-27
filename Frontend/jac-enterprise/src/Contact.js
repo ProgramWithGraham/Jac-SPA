9d88df1687580fbbdf078af66ef7d6a04448fad9
tlc67 <kris7cuevas@gmail.com>
2024-03-27 18:02:34 -0400

    partial updates

--- Frontend/jac-enterprise/src/Contact.js
+++ Frontend/jac-enterprise/src/Contact.js
@@ -3,8 +3,22 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h2>Contact</h2>
-      <p>This is the Contact page.</p>
+      <header>
+        <h3>Contact Information</h3>
+      </header>
+      <section id="contactInformation">
+        <aside class="sidebar">
+          <ul>
+            <li>Kristina Cuevas</li>
+            <li>123 Address</li>
+            <li>216-555-8090</li>
+            <li>abcd@gmail.com</li>
+          </ul>
+        </aside>
+        <a href="https://www.linkedin.com/in/kristina-cuevas-b13bb92a8/">
+          <img src="./images/Linkedin.png" alt="LinkedIn" />
+        </a>
+      </section>
     </div>
   );
 }
