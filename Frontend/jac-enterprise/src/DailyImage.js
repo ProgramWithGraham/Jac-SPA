f9bd5303d900582ee1b108050253da9438a6b90e
tlc67 <kris7cuevas@gmail.com>
2024-03-26 23:11:54 -0400

    basic working  code

---
+++ Frontend/jac-enterprise/src/DailyImage.js
@@ -0,0 +1,43 @@
+import React from "react";
+export default class DailyImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+      explanation: "",
+    };
+    this.fetchDailyImage = this.fetchDailyImage.bind(this);
+  }
+  fetchDailyImage() {
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=SAJF3WueHZsrUhnD47Ikt8PaZu800EOw6VMlVxvE"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          url: data.url,
+          explanation: data.explanation,
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchDailyImage();
+  }
+
+  render() {
+    let url = this.state.url;
+    let explanation = this.state.explanation;
+
+    return (
+      <div>
+        <img
+          src={url}
+          alt="daily image from NASA"
+          style={{ width: 350, height: 350 }}
+        />
+        <p>{explanation}</p>
+      </div>
+    );
+  }
+}
