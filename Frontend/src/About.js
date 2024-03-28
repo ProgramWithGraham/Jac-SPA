fb51a601f568a0aada358aff1f2e27d6d2a45efb
ProgramWithGraham <gaps512@gmail.com>
2024-03-27 22:25:16 -0500

    a

--- Frontend/src/About.js
+++ Frontend/src/About.js
@@ -23,6 +23,27 @@ function About() {
           camp, and will finish my training in a few more weeks. I am genuinely
           excited for what the future holds.
         </p>
+        </section>
+
+        <header>
+        <img src="./" alt="profile" />
+        <h3>Kristina Cuevas</h3>
+      </header>
+
+      <section id="aboutMe">
+        <p>
+          My experience as a catering director has developed my strong customer
+          service skills. It has also taught me that hard work, meticulous
+          attention to detail, and communication, are the keys to great customer
+          service. I was introduced to the world of technology and coding by my
+          adult children, who have been an integral motivation in my decision to
+          change the trajectory of my life and pursue a career in technology. I
+          am looking forward to transitioning my love of customer service and
+          working as part of a team, into a successful new career as a software
+          developer. I am currently enrolled in a highly intensive coding boot
+          camp, and will finish my training in a few more weeks. I am genuinely
+          excited for what the future holds.
+        </p>
       </section>
     </div>
   );
