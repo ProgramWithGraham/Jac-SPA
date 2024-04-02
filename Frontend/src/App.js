f334650878cfe4e60c90bb42d3e30b22fb015d9e
tcollert <tcollert1@gmail.com>
2024-04-02 11:26:24 -0400

    changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import DailyImage from "./pages/DailyImage";
+import DailyImage from "./components/DailyImage";
 import FavoritesButton from "./components/FavoritesButton";
 import FavoritesList from "./components/FavoriteList";
 import SaveButton from "./components/SaveButton";
@@ -11,39 +11,47 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import SecondAPI from "./pages/SecondAPI";
 import ThirdAPI from "./pages/ThirdAPI";
 import setFavorites from "./components/FavoritesButton";
-import setExplanation from "./pages/DailyImage";
+import setExplanation from "./components/DailyImage";
 import favorites from "./components/FavoriteList";
 
 function App() {
+  const [dailyImage, setDailyImage] = useState();
+
   return (
-    <Router>
-      <div className="container">
-        return (
-        <nav className="nav">
-        <Link to="/" className="site-title">
-            AstroKids Adventures
-          </Link>
-          <ul>
-            <li>
-              <Link to="/Home">Home</Link>
-            </li>
-            <li>
-              <Link to="/DailyImage">NASA DAILY IMAGE</Link>
-            </li>
-            <li>
-              <Link to="SecondAPI">SECOND API</Link>
-            </li>
-            <li>
-              <Link to="ThirdAPI">THIRD API</Link>
-            </li>
-            <li>
-              <Link to="/About">ABOUT</Link>
-            </li>
-            <li>
-              <Link to="/Contact">CONTACT</Link>
-            </li>
-          </ul>
-        </nav>
+    <div className="container">
+      <Router>
+        <header>
+          <div>
+            <nav className="nav">
+              <ul>
+                <li>
+                  <Link to="/" className="site-title">
+                    AstroKids Adventures
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/DailyImage">NASA DAILY IMAGE</Link>
+                </li>
+                <li>
+                  <Link to="SecondAPI">SECOND API</Link>
+                </li>
+                <li>
+                  <Link to="ThirdAPI">THIRD API</Link>
+                </li>
+                <li>
+                  <Link to="/About">ABOUT</Link>
+                </li>
+                <li>
+                  <Link to="/Contact">CONTACT</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/DailyImage" element={<DailyImage />} />
@@ -51,13 +59,13 @@ function App() {
           <Route path="/ThirdAPI" element={<ThirdAPI />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
-          <FavoritesButton setFavorites={setFavorites} />
-          <DailyImage set={setExplanation} />
-          <FavoritesList favorites={favorites} />
         </Routes>
-      </div>
+        <FavoritesButton setFavorites={setFavorites} />
+        <DailyImage set={setExplanation} />
+        <FavoritesList favorites={favorites} />
+      </Router>
       <footer class="copyright">Ctrl Alt Elite Copyright &copy; 2024</footer>
-    </Router>
+    </div>
   );
 }
 export default App;
