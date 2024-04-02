68324773605f11efff8923ccfad648fec43435c9
tlc67 <kris7cuevas@gmail.com>
2024-04-02 19:25:15 -0400

    change

---
+++ Frontend/src/pages/Contact.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <header>
+        <h2>Contact Information</h2>
+
+        <section id="contactInformation">
+          <h3>Kristina Cuevas</h3>
+
+          <a href="https://www.linkedin.com/in/kristina-cuevas-b13bb92a8/">
+            <img
+              src="./images/Linkedin.png"
+              alt="LinkedIn"
+              width="10"
+              height="10"
+            />
+          </a>
+          <a href="https://github.com/tlc67" width="10" height="10">
+            <img src="./images/Github.png" alt="Github" />
+          </a>
+
+          <h3>Tarrah Collert</h3>
+          <a href="https://www.linkedin.com/in/tarrah-collert-472777141/">
+            <img src="./images/Linkedin.png" alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/tcollert">
+            <img src="./images/Github.png" alt="Github" />
+          </a>
+
+          <h3>Donald Fletcher</h3>
+          <a href="https://www.linkedin.com/in/donald-fletcher-jr-2973992b5/">
+            <img
+              src="./images/Linkedin.png"
+              alt="LinkedIn"
+              width="10"
+              height="10"
+            />
+          </a>
+          <a href="https://github.com/donaldfletcher11/donaldfletcher11.github.io">
+            <img
+              src="./images/Github.png"
+              alt="Github"
+              width="10"
+              height="10"
+            />
+          </a>
+
+          <h3>Adrian Graham</h3>
+          <a href="https://www.linkedin.com/in/adrian-graham-b9750419/">
+            <img
+              src="./images/Linkedin.png"
+              alt="LinkedIn"
+              width="10"
+              height="10"
+            />
+          </a>
+          <a href="https://github.com/ProgramWithGraham/ProgramWithGraham.github.io">
+            <img
+              src="./images/Github.png"
+              alt="Github"
+              width="10"
+              height="10"
+            />
+          </a>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default Contact;
