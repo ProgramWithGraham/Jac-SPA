61cab5dda687b3fbd168a88ad1e43b12ae98b215
tlc67 <kris7cuevas@gmail.com>
2024-04-04 06:30:53 -0400

    created random fact button

--- Frontend/src/components/RandomFactButton.js
+++ Frontend/src/components/RandomFactButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function RandomFactButton({ setRandomFact }) {
   const fetchRandomFact = () => {
-    fetch("http://api.fungenerators.com/fact/random?")
+    fetch("")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -13,7 +13,7 @@ function RandomFactButton({ setRandomFact }) {
         setRandomFact(data.value);
       })
       .catch((error) => {
-        console.error("There was a problem fetching the joke:", error);
+        console.error("There was a problem fetching the fact:", error);
       });
   };
   return <button onClick={fetchRandomFact}>Get Random Fact</button>;
