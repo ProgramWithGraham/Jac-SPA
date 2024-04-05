1e686b83852442b0f5af222e365ce1e25bae7ae7
tcollert <tcollert1@gmail.com>
2024-04-05 02:15:11 -0400

    Updated CSS

--- Frontend/src/pages/Contact.js
+++ Frontend/src/pages/Contact.js
@@ -4,35 +4,36 @@ function Contact() {
   return (
     <div>
       <header>
-        <h2>Contact Information</h2>
+        <img src="./images/AdobeStock_139302582.jpeg" alt="background" id="background-image" width="100%" height="100%"/>
+        <h2 id="CI">Contact Information</h2>
         <section id="contactInformation">
           <h3>Kristina Cuevas</h3>
           <a href="https://www.linkedin.com/in/kristina-cuevas-b13bb92a8/">
-            <img src="./images/Linkedin.png" alt="LinkedIn" />
+            <img src="./images/Linkedin.png" alt="LinkedIn" id="LI1"/>
           </a>
           <a href="https://github.com/tlc67">
-            <img src="./images/Github.png" alt="Github" />
+            <img src="./images/Github.png" alt="Github" id="GH1" />
           </a>
           <h3>Tarrah Collert</h3>
           <a href="https://www.linkedin.com/in/tarrah-collert-472777141/">
-            <img src="./images/Linkedin.png" alt="LinkedIn" />
+            <img src="./images/Linkedin.png" alt="LinkedIn" id="LI2" />
           </a>
           <a href="https://github.com/tcollert">
-            <img src="./images/Github.png" alt="Github" />
+            <img src="./images/Github.png" alt="Github" id="GH2" />
           </a>
           <h3>Donald Fletcher</h3>
           <a href="https://www.linkedin.com/in/donald-fletcher-jr-2973992b5/">
-            <img src="./images/Linkedin.png" alt="LinkedIn" />
+            <img src="./images/Linkedin.png" alt="LinkedIn" id="LI3" />
           </a>
           <a href="https://github.com/donaldfletcher11/donaldfletcher11.github.io">
-            <img src="./images/Github.png" alt="Github" />
+            <img src="./images/Github.png" alt="Github"id="GH3" />
           </a>
           <h3>Adrian Graham</h3>
           <a href="https://www.linkedin.com/in/adrian-graham-b9750419/">
-            <img src="./images/Linkedin.png" alt="LinkedIn" />
+            <img src="./images/Linkedin.png" alt="LinkedIn" id="LI4"/>
           </a>
           <a href="https://github.com/ProgramWithGraham/ProgramWithGraham.github.io">
-            <img src="./images/Github.png" alt="Github" />
+            <img src="./images/Github.png" alt="Github" id="GH4" />
           </a>
         </section>
       </header>
